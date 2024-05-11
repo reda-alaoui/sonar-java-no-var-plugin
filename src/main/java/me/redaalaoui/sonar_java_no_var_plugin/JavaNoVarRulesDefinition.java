97648665f4edc1ab128e273c4cf404672b92bb57
Réda Housni Alaoui <reda-alaoui@hey.com>
2024-05-11 17:39:30 +0200

    Upgrade to latest LTS 9.9

--- src/main/java/me/redaalaoui/sonar_java_no_var_plugin/JavaNoVarRulesDefinition.java
+++ src/main/java/me/redaalaoui/sonar_java_no_var_plugin/JavaNoVarRulesDefinition.java
@@ -1,18 +1,25 @@
 package me.redaalaoui.sonar_java_no_var_plugin;
 
 import java.util.Collections;
+import org.sonar.api.SonarRuntime;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonarsource.analyzer.commons.RuleMetadataLoader;
 
 /** @author Réda Housni Alaoui */
 public class JavaNoVarRulesDefinition implements RulesDefinition {
 
+  private final SonarRuntime sonarRuntime;
+
+  public JavaNoVarRulesDefinition(SonarRuntime sonarRuntime) {
+    this.sonarRuntime = sonarRuntime;
+  }
+
   @Override
   public void define(Context context) {
     NewRepository repository =
         context.createRepository(JavaNoVarRepository.KEY, "java").setName("Java No Var Repository");
 
-    new RuleMetadataLoader("me/redaalaoui/sonar_java_no_var_plugin/rules")
+    new RuleMetadataLoader("me/redaalaoui/sonar_java_no_var_plugin/rules", sonarRuntime)
         .addRulesByAnnotatedClass(repository, Collections.<Class<?>>singletonList(NoVarRule.class));
 
     repository.done();
